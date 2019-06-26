cbe495c4a2043bd3f8ac63e7c0becbc571efe95f
Damilola <46329891+damildrizzy@users.noreply.github.com>
2019-06-26 09:39:58 +0100

    Add files via upload

---
+++ myshop/shop/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+    
+    class meta:
+        ordering = ('name',)
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
+    
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available =models.BooleanField(default=True)
+    #auto_now_add updates the value with the time and date of creation of record
+    created = models.DateTimeField(auto_now_add=True)
+    #auto_now updates the value of field to current time and date everytime the Model.save() is called
+    updated = models.DateTimeField(auto_now=True)
+
+    class meta:
+        ordering = 'name'
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('shop:product_detail', args=[self.id, self.slug])
+
+
+
+
+
+
+
+
